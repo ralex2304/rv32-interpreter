8fedd151bfa39815e78a93c643859dab3a7fdb92
ralex2304 <ralex2304@gmail.com>
2025-10-24 08:30:14 +0300

    Add echo end to end test

--- assets/api.h
+++ assets/api.h
@@ -1,6 +1,17 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern long read( int fd, char* data, long maxlen);
 extern long write( int fd, const char* data, long len);
-extern __attribute__((noreturn)) void exit( long status);
+
+#ifdef __cplusplus
+}
+#endif
+
+#define stdin 0
+#define stdout 1
+#define stderr 2
 
--- tests/end_to_end/test_runner.py
+++ tests/end_to_end/test_runner.py
@@ -7,6 +7,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("test_directory", help="directory with test executable and config")
 parser.add_argument("-i", "--interpreter", default="../../build/rv32-interpreter",
                                       help="interpreter executable path")
+parser.add_argument("--no-silent-make", action='store_true', default=False,
+                                      help="enable GNU make output")
 
 args = parser.parse_args()
 directory = args.test_directory
@@ -16,8 +18,9 @@ with open(directory + "/build_config.txt") as build_config:
     marches = build_config.read().split()
 
     for march in marches:
-        build_res = subprocess.run(["make", "--silent", f"{directory}/build/test_{march}",
-                                   f"SRC_DIR={directory}", f"ARCH={march}"])
+        build_res = subprocess.run(["make", f"{directory}/build/test_{march}",
+                                   f"SRC_DIR={directory}", f"ARCH={march}"] +
+                                   (["--silent"] if not args.no_silent_make else []))
 
         if build_res.returncode != 0:
             print(f"[FAIL] [BUILD_FAIL] Failed to build for -march={march}")
